6e95c61ce77114330a9d2e1f1db79fc3433e26b3
root <paulhaolee@gmail.com>
2020-07-05 20:35:13 +0000

    3

--- src/chatgui.h
+++ src/chatgui.h
@@ -1,5 +1,6 @@
 #ifndef CHATGUI_H_
 #define CHATGUI_H_
+#include <memory>
 
 #include <wx/wx.h>
 
@@ -15,8 +16,8 @@ private:
 
     //// STUDENT CODE
     ////
-
-    ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic> _chatLogic;
+    // ChatLogic *_chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +28,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
