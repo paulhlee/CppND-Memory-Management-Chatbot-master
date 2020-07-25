b12bcff97c7830b268d7c74e1eb9718643976fc8
root <paulhaolee@gmail.com>
2020-07-25 13:47:14 +0000

    3

--- src/graphnode.h
+++ src/graphnode.h
@@ -17,6 +17,7 @@ private:
 
     // data handles (owned)
     std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    std::vector<GraphEdge *> _childEdgesss;  // edges to subsequent nodes
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
